d02df7ba4677ab907a0f44468211ba849045a11a
Diana Robles <dana_rg@hotmail.com>
2024-02-11 16:04:33 -0600

    crear HTML

---
+++ app.js
@@ -0,0 +1,22 @@
+let titulo= document.querySelector('h1');
+titulo.innerHTML = 'Coloca tu frase a encriptar';
+
+
+
+function cifrarTextoDeUsuario(){
+    let generarEncriptacion = parseInt(document.getElementById('textoUsuario').value);
+    //console.log(value(generarEncriptacion));
+    console.log(typeof(generarEncriptacion));
+    // $("#div_domi").load("buscar.php?id="+$("#desencriptado put").attr('value'))
+    return 
+}
+
+
+
+    
+    
+    //textarea desencriptado
+
+   
+//if textoUsuario (if "e" then "enter", if "i" then "imes", if "a" then "ai", if "o" then "ober", if "u" then "ufat")
+    //alert('click desde el boton')
